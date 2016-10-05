adb9596e47e91a1dfa337ef2e53ab40496d12124
s1310238069@students.fh-hagenberg.at <michaeltroger@users.noreply.github.com>
2016-10-06 00:45:29 +0200

    init

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # home-esp8266-door
+
+This is an Arduino project for use with the ESP8266 in form of a NodeMCU. It is part of the DIY home surveillance with a Raspberry Pi as server https://github.com/michaeltroger/home
+
+This program detects a door opening via a connected magnetic switch and sends a request to the web server when the door opened.
+
+create a file named `wificonf.h` with your wifi login credentials like:
+
+`const char[] WIFI_SSID = "your-ssid";
+const char[] WIFI_PSK = "your password";`
---
+++ home-esp8266-door.ino
@@ -0,0 +1,56 @@
+/**
+Copyright 2016 Michael Troger <https://michaeltroger.com>
+This file is part of Home Surveillance with Node.js.
+
+Home Surveillance with Node.js is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Home Surveillance with Node.js is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Home Surveillance with Node.js.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+
+#include "wificonf.h"
+
+// Remote site information
+const char[] hostname = "192.168.1.161";
+const int port = 3000;
+
+WiFiClient client;
+
+// door sensor
+const int doorPin = 14;
+#define CLOSED 0
+#define OPENED 1
+int doorState = CLOSED;
+
+void setup() {
+  WiFi.begin(WIFI_SSID, WIFI_PSK);
+}
+
+void loop() {
+  if(digitalRead(doorPin) == HIGH && doorState == OPENED) { 
+      doorState = CLOSED;
+  }
+  if(digitalRead(doorPin) == LOW && doorState == CLOSED) { 
+      doorState = OPENED;
+      
+      client.connect(hostname, port);
+      
+      // do a HTTP get request
+      client.println("GET /door HTTP/1.1");
+      client.print("Host: ");
+      client.println(hostname);
+      client.println("Connection: close");
+      client.println();
+  }
+}
+
